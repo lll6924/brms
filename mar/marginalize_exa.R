23f0651865e51fca282c89159a27f6d93b57ee9d
Jinlin Lai <lll6924@qq.com>
2024-11-05 13:08:42 -0500

    backward

--- mar/marginalize_exa.R
+++ mar/marginalize_exa.R
@@ -1,6 +1,6 @@
 library(devtools)
 load_all('../')
 fit1 <- brm(time ~ age + (age+1|disease*sex) + (1|patient),
-            data = kidney, family = "gaussian",   prior = set_prior("cauchy(0,2)", class = "sd"), marginalize = 'disease:sex')
+            data = kidney, family = "gaussian",   prior = set_prior("cauchy(0,2)", class = "sd"),)
 summary(fit1)
 stancode(fit1)
